6b70902e9dd1b8f207fda968bccdd6645b2281ea
BulatBubeev <strofantink@gmail.com>
2024-05-23 22:07:50 +0800

    debug toolbar

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
     "main",
     "goods",
 ]
@@ -33,6 +34,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
@@ -95,3 +97,7 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from app.settings import DEBUG
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("main.urls")),
     path("catalog/", include("goods.urls")),
 ]
+
+if DEBUG:
+    urlpatterns += [
+        path("__debug__/", include("debug_toolbar.urls")),
+    ]
